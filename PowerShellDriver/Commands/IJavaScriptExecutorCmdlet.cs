e6006a1ee9bc7630b19649558c0d2e6eec684cca
Matt Black <matt.black7@gmail.com>
2014-08-03 00:01:17 -0700

    Initial implementation of Invoke-JavaScriptExecution -- async now untested

---
+++ PowerShellDriver/Commands/IJavaScriptExecutorCmdlet.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Management.Automation;
+
+using OpenQA.Selenium;
+
+namespace PowerShellDriver.Commands
+{
+    [Cmdlet(VerbsLifecycle.Invoke, "JavaScriptExecution")]
+    public class IJavaScriptExecutorCmdlet : PSCmdlet
+    {
+        /// <summary>
+        /// An object that implements <see cref="IJavaScriptExecutor"/>
+        /// </summary>
+        [Parameter(Mandatory=true,
+            ValueFromPipeline=true)]
+        public IJavaScriptExecutor JSExecutor
+        {
+            get { return _jsExecutor; }
+            set { _jsExecutor = value; }
+        }
+        private IJavaScriptExecutor _jsExecutor;
+
+        /// <summary>
+        /// The JavaScript code to execute
+        /// </summary>
+        [Parameter(Mandatory=true,
+            Position=0)]
+        public String Script
+        {
+            get { return _script; }
+            set { _script = value; }
+        }
+        private String _script;
+
+        /// <summary>
+        /// The arguments to the script
+        /// </summary>
+        [Parameter(Mandatory=false,
+            Position=1)]
+        public Object[] Args
+        {
+            get { return _args; }
+            set { _args = value; }
+        }
+        private Object[] _args = new Object[0];
+
+        /// <summary>
+        /// Execute the script asynchronously
+        /// </summary>
+        [Parameter(Mandatory=false)]
+        public SwitchParameter Async
+        {
+            get { return _async; }
+            set { _async = value; }
+        }
+        private bool _async;
+
+        [Parameter(Mandatory=false)]
+        public SwitchParameter PassThru
+        {
+            get { return _passThru; }
+            set { _passThru = value; }
+        }
+        private bool _passThru;
+
+        protected override void ProcessRecord()
+        {
+            object returnVal = (_async) ?
+                _jsExecutor.ExecuteAsyncScript(_script, _args) :
+                _jsExecutor.ExecuteScript(_script, _args);
+
+            if (_passThru)
+                WriteVarAndPassThru(returnVal, _jsExecutor);
+            else
+                this.WriteObject(returnVal);
+        }
+
+        #region Private support methods
+        private void WriteVarAndPassThru(object variable, object passThruVar)
+        {
+            this.SessionState.PSVariable.Set("script_result", variable);
+            this.WriteObject(passThruVar);
+            return;
+        }
+        #endregion
+    }
+}
---
+++ Test/TestScripts/IJavaScriptExecutorCmdlet.Tests.ps1
@@ -0,0 +1,30 @@
+﻿Describe "Invoke-JavaScriptExecution" {
+    #NEED TO ADD ASYNC TESTS
+    Context "Test sync and async execution" {
+        #setup the driver
+        $htmlUnitCaps = [OpenQA.Selenium.Remote.DesiredCapabilities]::HtmlUnitWithJavaScript()
+        $driver = New-Object OpenQA.Selenium.Remote.RemoteWebDriver -ArgumentList @($htmlUnitCaps)
+        $driver.Navigate().GoToUrl("file:///C:/Users/Matt/dotNet/PowerShellDriver/Test/Resources/testpage.html")   #load the test page
+        $driver.Manage().Timeouts().SetScriptTimeout([System.TimeSpan]::FromSeconds(2))
+
+        It "executes javascript w/o parameters synchronously" {
+            $result_dotNet = ([OpenQA.Selenium.IJavaScriptExecutor]$driver).ExecuteScript("return document.title")
+            $result_poSh = $driver | Invoke-JavaScriptExecution "return document.title"
+            $result_poSh | Should Be $result_dotNet
+        }
+        <#It "executes javascript w/o parameters asynchronously" {
+            $result_dotNet = ([OpenQA.Selenium.IJavaScriptExecutor]$driver).ExecuteAsyncScript("return document.title")
+            $result_poSh = $driver | Invoke-JavaScriptExecution "return document.title" -Async
+            $result_poSh | Should Be $result_dotNet
+        }#>
+        $elem = $driver.FindElementByTagName("h1")
+        It "executes javascript w/ parameters synchronously" {
+            $driver | Invoke-JavaScriptExecution "arguments[0].color='blue'" @($elem)
+            $elem.GetCssValue("color") | Should Be "blue"
+        }
+        <#It "executes javascript w/ parameters asynchrously" {
+            $driver | Invoke-JavaScriptExecution "arguments[0].color='red'" @($elem) -Async
+            $elem.GetCssValue("color") | Should Be "red"
+        }#>
+    }
+}
