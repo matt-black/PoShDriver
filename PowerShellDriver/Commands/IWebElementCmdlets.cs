fd534a756f016715411ee9feed574c7f41edffe5
Matt Black <matt.black7@gmail.com>
2014-08-02 18:23:53 -0700

    Refactor the Get-WebElementProperty Cmdlet. Some tests failing but results look okay and cmdlet is much cleaner

--- PowerShellDriver/Commands/IWebElementCmdlets.cs
+++ PowerShellDriver/Commands/IWebElementCmdlets.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Management.Automation;
 
@@ -103,43 +104,229 @@ namespace PowerShellDriver.Commands
         private bool _all;
         #endregion
 
+        /// <summary>
+        /// Indicates what to write to the pipeline
+        /// If true, writes the <see cref="IWebElement"/> input to the pipeline
+        /// If false (unspecified), it writes the property to the pipeline
+        /// </summary>
+        public SwitchParameter PassThru
+        {
+            get { return _passThru; }
+            set { _passThru = value; }
+        }
+        private bool _passThru = false;
+
+        protected override void ProcessRecord()
+        {
+            var propertyValue = GetElementProperty(this.ParameterSetName);
+            if (_passThru)
+            {
+                this.WritePropToVariable();
+            }
+            else
+            {
+                this.WriteObject(propertyValue);
+                return;
+            }
+            this.WriteObject(_webElement);
+        }
+
+        #region Private support methods
+        /// <summary>
+        /// Generate a <see cref="Dictionary<string,object>"/> of all the properties of the <see cref="IWebElement"/> that
+        /// this Cmdlet had passed to it
+        /// </summary>
+        /// <returns>A dictionary of properties</returns>
+        private Dictionary<string, object> MakePropertiesDict()
+        {
+            Dictionary<string, object> propsDict = new Dictionary<string, object>();
+
+            var props = _webElement.GetType().GetProperties();
+            foreach (var prop in props)
+            {
+                propsDict.Add(prop.Name, prop.GetValue(_webElement, null));
+            }
+            return propsDict;
+        }
+
+        /// <summary>
+        /// Writes the property specified by the Cmdlet's parameter to a variable in the current runspace
+        /// </summary>
+        private void WritePropToVariable()
+        {
+            StringBuilder varName = new StringBuilder("elementProperty_");
+            varName.Append(this.ParameterSetName);
+            var varValue = GetElementProperty(this.ParameterSetName);
+            this.SessionState.PSVariable.Set(varName.ToString(), varValue);
+        }
+
+        /// <summary>
+        /// Gets the value of the specified property for the _webElement
+        /// </summary>
+        private object GetElementProperty(string p)
+        {
+            var props = _webElement.GetType().GetProperties();
+            var value = props
+                .Where(prop => prop.Name == p)
+                .Select(prop => prop.GetValue(_webElement, null));
+            return value;
+        }
+        #endregion
+    }
+
+    [Cmdlet(VerbsLifecycle.Invoke, "WebElementMethod")]
+    public class InvokeWebElementMethod : IWebElementCmdlet, IDynamicParameters
+    {
+        private bool _reqStringParam = false;
+
+        #region SwitchParams for different methods
+        [Parameter(ParameterSetName="Clear",
+            Mandatory=true)]
+        public SwitchParameter Clear
+        {
+            get { return _clear; }
+            set { _clear = value; }
+        }
+        private bool _clear;
+
+        [Parameter(ParameterSetName="Click",
+            Mandatory=true)]
+        public SwitchParameter Click
+        {
+            get { return _click; }
+            set { _click = value; }
+        }
+        private bool _click;
+
+        [Parameter(ParameterSetName="GetAttribute",
+            Mandatory=true)]
+        public SwitchParameter GetAttribute
+        {
+            get { return _getAttribute; }
+            set
+            {
+                if (value == true)
+                {
+                    _reqStringParam = true;
+                }
+                _getAttribute = value;
+            }
+        }
+        private bool _getAttribute;
+
+        [Parameter(ParameterSetName="GetCssValue",
+            Mandatory=true)]
+        public SwitchParameter GetCssValue
+        {
+            get { return _getCssValue; }
+            set
+            {
+                if (value == true)
+                {
+                    _reqStringParam = true;
+                }
+                _getCssValue = value;
+            }
+        }
+        private bool _getCssValue;
+
+        [Parameter(ParameterSetName="SendKeys",
+            Mandatory=true)]
+        public SwitchParameter SendKeys
+        {
+            get { return _sendKeys; }
+            set
+            {
+                if (value == true)
+                {
+                    _reqStringParam = true;
+                }
+                _sendKeys = value;
+            }
+        }
+        private bool _sendKeys;
+
+        [Parameter(ParameterSetName="Submit",
+            Mandatory=true)]
+        public SwitchParameter Submit
+        {
+            get { return _submit; }
+            set { _submit = value; }
+        }
+        private bool _submit;
+        #endregion
+
+        /// <summary>
+        /// Indicates what to write to the pipeline
+        /// If true, writes the <see cref="IWebElement"/> input to the pipeline
+        /// If false (unspecified), it writes the method output to the pipeline 
+        /// </summary>
+        [Parameter(Mandatory = false)]
+        public SwitchParameter PassThru
+        {
+            get { return _passThru; }
+            set { _passThru = value; }
+        }
+        private bool _passThru;
+
+        /// <summary>
+        /// Gets the dynamic <see cref="StringParam"/> if the method
+        /// requires it
+        /// </summary>
+        /// <returns></returns>
+        public object GetDynamicParameters()
+        {
+            if (_reqStringParam)
+            {
+                _stringParam = new StringParam();
+                return _stringParam;
+            }
+            return null;
+        }
+        private StringParam _stringParam;
+
+        /// <summary>
+        /// A class representing a single string parameter for a <see cref="IWebElement"/> member method
+        /// </summary>
+        public class StringParam
+        {
+            [Parameter(Mandatory=true, 
+                Position=1)]
+            [ValidateNotNull]
+            public String Parameter
+            {
+                get { return strParam; }
+                set { strParam = value; }
+            }
+            private String strParam;
+        }
+
         protected override void ProcessRecord()
         {
             switch (this.ParameterSetName)
             {
-                case "Displayed":
-                    this.WriteObject(_webElement.Displayed);
+                case "Clear":
+                    _webElement.Clear();
                     return;
-                case "Enabled":
-                    this.WriteObject(_webElement.Displayed);
+                case "Click":
+                    _webElement.Click();
                     return;
-                case "Location":
-                    this.WriteObject(_webElement.Location);
+                case "GetAttribute":
+                    this.WriteObject(
+                        _webElement.GetAttribute(_stringParam.Parameter));
                     return;
-                case "Selected":
-                    this.WriteObject(_webElement.Selected);
+                case "GetCssValue":
+                    this.WriteObject(
+                        _webElement.GetCssValue(_stringParam.Parameter));
                     return;
-                case "Size":
-                    this.WriteObject(_webElement.Size);
+                case "SendKeys":
+                    _webElement.SendKeys(_stringParam.Parameter);
                     return;
-                case "TagName":
-                    this.WriteObject(_webElement.TagName);
-                    return;
-                case "Text":
-                    this.WriteObject(_webElement.Text);
-                    return;
-                case "All":
-                    Dictionary<string, object> propsDict = new Dictionary<string, object>();
-
-                    var props = _webElement.GetType().GetProperties();
-                    foreach (var prop in props)
-                    {
-                        propsDict.Add(prop.Name, prop.GetValue(_webElement, null));
-                    }
-                    this.WriteObject(new Hashtable(propsDict));
+                case "Submit":
+                    _webElement.Submit();
                     return;
                 default:
-                    throw new Exception("did not specify a valid parameterset");
+                    throw new Exception("Invalid method specified");
             }
         }
     }
